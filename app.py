d2c4663b1e0e055fe7d897bb313a9dbefc541d4c
nguy2625 <nguy2625@umn.edu>
2020-02-19 20:03:44 -0600

    Master

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, Response, render_template
+import requests
+import itertools
+from flask_wtf.csrf import CSRFProtect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import Regexp, ValidationError, Optional
+import re
+from flask.json import jsonify
+
+csrf = CSRFProtect()
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "row the boat"
+csrf.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
