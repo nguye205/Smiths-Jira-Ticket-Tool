36e02b2193fcc5760e6cad86a1ada6266c57ab1d
nguye205 <33646714+nguye205@users.noreply.github.com>
2020-05-08 16:20:22 -0500

    working on api

--- static/js/getInput.js
+++ static/js/getInput.js
@@ -1,3 +1,6 @@
+var username;
+var api = 'cEM3kVmmC9m0YMZvgFvD1D2F';
+
 function FormValidation(inputToValidate){
   console.log(inputToValidate.value);
   if (inputToValidate.value != "" && inputToValidate.value != "-"){
@@ -9,7 +12,8 @@ function FormValidation(inputToValidate){
     inputToValidate.style.boxShadow = "0 0 6px red, 0 0 2px";
   }
 }
-////////////////////////////////////////////////////////////////////////////
+
+
 
 var searchNews = 'https://api.currentsapi.services/v1/search?' +
      'keywords=Apple&language=en&' +
@@ -17,6 +21,8 @@ var searchNews = 'https://api.currentsapi.services/v1/search?' +
 
 var latestNews = 'https://api.currentsapi.services/v1/latest-news?' +
      'apiKey=OIsqDsDGS2GagUTvcQ5V63nG6osMGg2zbKQbXn_QaWY71_7Z';
+
+var jiraTicket = 'https://your-domain.atlassian.net/rest/api/3/issue/DEMO-1'
 
 const myList = document.querySelector('#newsList');
 
@@ -44,4 +50,7 @@ async function postData() {
                console.log(data);
           }); //then data
 }
+curl -u "username:password" http://device-name:port/rpc
+-d "<get-interface-information/><get-software-information format='text/plain'/>"
+—header "Accept: application/json"
 window.onload = postData;
