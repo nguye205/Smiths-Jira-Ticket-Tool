783cc2f51ce801e6dbfe58512fa46dd9718afde6
nguye205 <nguye205@gmail.com>
2020-05-07 22:28:40 -0500

    update validation

--- static/js/getInput.js
+++ static/js/getInput.js
@@ -1,10 +1,12 @@
 function FormValidation(inputToValidate){
   console.log(inputToValidate.value);
-  if (inputToValidate.value != ""){
+  if (inputToValidate.value != "" && inputToValidate.value != "-"){
+    console.log('valid');
     inputToValidate.style.borderColor = "green";
     inputToValidate.style.borderWidth = "thin";
   }
   else {
+    console.log('invalid');
     inputToValidate.style.borderColor = "red";
     inputToValidate.style.borderWidth = "medium";
   }
