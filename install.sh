1ef483716faa2b6ceae61bbc97d018c740f28e3d
nguye205 <nguye205@gmail.com>
2020-05-07 23:08:09 -0500

    add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install python3 --pre
+py -3 -m venv venv
+venv\Scripts\activate
+pip install Flask
+pip install -r requirements.txt
---
+++ openWebsite.sh
@@ -0,0 +1,5 @@
+cd /Users/techyedgy/Desktop/nguye205-GIT/Dalco-ServiceRequestWebsite
+. venv/bin/activate
+export FLASK_ENV=development
+flask run
+open -a "Google Chrome" https://125.0.0.1:5000
