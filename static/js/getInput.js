e490de073d95b1378b99f10d0f07438bb2737139
nguye205 <33646714+nguye205@users.noreply.github.com>
2020-05-08 13:36:41 -0500

    add shadow to borders

--- static/js/getInput.js
+++ static/js/getInput.js
@@ -2,13 +2,11 @@ function FormValidation(inputToValidate){
   console.log(inputToValidate.value);
   if (inputToValidate.value != "" && inputToValidate.value != "-"){
     console.log('valid');
-    inputToValidate.style.borderColor = "green";
-    inputToValidate.style.borderWidth = "thin";
+    inputToValidate.style.boxShadow = "0 0 6px green, 0 0 2px";
   }
   else {
     console.log('invalid');
-    inputToValidate.style.borderColor = "red";
-    inputToValidate.style.borderWidth = "medium";
+    inputToValidate.style.boxShadow = "0 0 6px red, 0 0 2px";
   }
 }
 ////////////////////////////////////////////////////////////////////////////
