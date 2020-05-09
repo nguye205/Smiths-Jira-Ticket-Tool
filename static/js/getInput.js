72055abafc312e7f4bd9189485a6e2762a5dd03d
nguye205 <nguye205@gmail.com>
2020-05-09 00:16:16 -0500

    asdf

--- static/js/getInput.js
+++ static/js/getInput.js
@@ -1,56 +1,155 @@
-var username;
+var username = 'khai.nguyen@smiths-medical.com';
 var api = 'cEM3kVmmC9m0YMZvgFvD1D2F';
 
-function FormValidation(inputToValidate){
+function FormValidation(inputToValidate) {
   console.log(inputToValidate.value);
-  if (inputToValidate.value != "" && inputToValidate.value != "-"){
+  if (inputToValidate.value != "" && inputToValidate.value != "-") {
     console.log('valid');
     inputToValidate.style.boxShadow = "0 0 6px green, 0 0 2px";
-  }
-  else {
+  } else {
     console.log('invalid');
     inputToValidate.style.boxShadow = "0 0 6px red, 0 0 2px";
   }
 }
 
+const bodyData = `{
+  "update": {},
+  "fields": {
+    "summary": "Main order flow broken",
+    "parent": {
+      "key": "SAN"
+    },
+    "issuetype": {
+      "id": "10000"
+    },
+    "components": [
+      {
+        "id": "10000"
+      }
+    ],
+    "customfield_20000": "06/Jul/19 3:25 PM",
+    "customfield_40000": {
+      "type": "doc",
+      "version": 1,
+      "content": [
+        {
+          "type": "paragraph",
+          "content": [
+            {
+              "text": "Occurs on all orders",
+              "type": "text"
+            }
+          ]
+        }
+      ]
+    },
+    "customfield_70000": [
+      "jira-administrators",
+      "jira-software-users"
+    ],
+    "project": {
+      "id": "10000"
+    },
+    "description": {
+      "type": "doc",
+      "version": 1,
+      "content": [
+        {
+          "type": "paragraph",
+          "content": [
+            {
+              "text": "Order entry fails when selecting supplier.",
+              "type": "text"
+            }
+          ]
+        }
+      ]
+    },
+    "reporter": {
+      "id": "5b10a2844c20165700ede21g"
+    },
+    "fixVersions": [
+      {
+        "id": "10001"
+      }
+    ],
+    "customfield_10000": "09/Jun/19",
+    "priority": {
+      "id": "20000"
+    },
+    "labels": [
+      "bugfix",
+      "blitz_test"
+    ],
+    "timetracking": {
+      "remainingEstimate": "5",
+      "originalEstimate": "10"
+    },
+    "customfield_30000": [
+      "10000",
+      "10002"
+    ],
+    "customfield_80000": {
+      "value": "red"
+    },
+    "security": {
+      "id": "10000"
+    },
+    "environment": {
+      "type": "doc",
+      "version": 1,
+      "content": [
+        {
+          "type": "paragraph",
+          "content": [
+            {
+              "text": "UAT",
+              "type": "text"
+            }
+          ]
+        }
+      ]
+    },
+    "versions": [
+      {
+        "id": "10000"
+      }
+    ],
+    "duedate": "2019-05-11T00:00:00.000Z",
+    "customfield_60000": "jira-software-users",
+    "customfield_50000": {
+      "type": "doc",
+      "version": 1,
+      "content": [
+        {
+          "type": "paragraph",
+          "content": [
+            {
+              "text": "Could impact day-to-day work.",
+              "type": "text"
+            }
+          ]
+        }
+      ]
+    },
+    "assignee": {
+      "id": "5b109f2e9729b51b54dc274d"
+    }
+  }
+}`;
 
+function sendTicketToJira() {
+  var credential = username + ':' + api;
+  console.log(btoa(credential.toString()));
 
-var searchNews = 'https://api.currentsapi.services/v1/search?' +
-     'keywords=Apple&language=en&' +
-     'apiKey=OIsqDsDGS2GagUTvcQ5V63nG6osMGg2zbKQbXn_QaWY71_7Z';
-
-var latestNews = 'https://api.currentsapi.services/v1/latest-news?' +
-     'apiKey=OIsqDsDGS2GagUTvcQ5V63nG6osMGg2zbKQbXn_QaWY71_7Z';
-
-var jiraTicket = 'https://your-domain.atlassian.net/rest/api/3/issue/DEMO-1'
-
-const myList = document.querySelector('#newsList');
-
-async function postData() {
-     fetch(searchNews)
-          .then(response => response.json()) //get the response and convert to json object
-          .then(data => {
-               for (var i = 0; i < data.news.length; i++) {
-                    //////////////////
-                    var newsListItem = document.createElement('li');
-                    var newsLink = document.createElement('a');
-                    newsListItem.appendChild(newsLink); //add <a> tag to list
-                    newsLink.appendChild(document.createTextNode(data.news[i].title)); //add text to the link
-                    newsLink.title = JSON.stringify(data.news[i].title);
-                    newsLink.href = JSON.stringify(data.news[i].url);
-
-                    var newsPicture = document.createElement('img');
-                    newsListItem.appendChild(newsPicture);
-                    newsPicture.src = JSON.stringify(data.news[i].image);
-                    newsPicture.alt = JSON.stringify(data.news[i].description);
-                    myList.appendChild(newsListItem);
-                    /////////
-                    console.log(data.news[i].author);
-               } //news for loop
-               console.log(data);
-          }); //then data
+  const options = {
+    method: 'POST',
+    headers: {
+      'Authorization': `Basic `+btoa(credential.toString()),
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: bodyData
+  };
+  fetch('https://smithsfodddrge.atlassian.net/rest/api/3/issue', options);
 }
-curl -u "username:password" http://device-name:port/rpc
--d "<get-interface-information/><get-software-information format='text/plain'/>"
-—header "Accept: application/json"
-window.onload = postData;
