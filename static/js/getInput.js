081558a0ff153f4aaa7107387d0699cebb5d30b1
nguye205 <nguye205@gmail.com>
2020-05-09 00:29:54 -0500

    Update getInput.js

--- static/js/getInput.js
+++ static/js/getInput.js
@@ -16,39 +16,11 @@ const bodyData = `{
   "update": {},
   "fields": {
     "summary": "Main order flow broken",
-    "parent": {
-      "key": "SAN"
-    },
     "issuetype": {
-      "id": "10000"
+      "name": "Bug"
     },
-    "components": [
-      {
-        "id": "10000"
-      }
-    ],
-    "customfield_20000": "06/Jul/19 3:25 PM",
-    "customfield_40000": {
-      "type": "doc",
-      "version": 1,
-      "content": [
-        {
-          "type": "paragraph",
-          "content": [
-            {
-              "text": "Occurs on all orders",
-              "type": "text"
-            }
-          ]
-        }
-      ]
-    },
-    "customfield_70000": [
-      "jira-administrators",
-      "jira-software-users"
-    ],
     "project": {
-      "id": "10000"
+      "key": "SAN"
     },
     "description": {
       "type": "doc",
@@ -65,36 +37,10 @@ const bodyData = `{
         }
       ]
     },
-    "reporter": {
-      "id": "5b10a2844c20165700ede21g"
-    },
-    "fixVersions": [
-      {
-        "id": "10001"
-      }
-    ],
-    "customfield_10000": "09/Jun/19",
-    "priority": {
-      "id": "20000"
-    },
     "labels": [
       "bugfix",
       "blitz_test"
     ],
-    "timetracking": {
-      "remainingEstimate": "5",
-      "originalEstimate": "10"
-    },
-    "customfield_30000": [
-      "10000",
-      "10002"
-    ],
-    "customfield_80000": {
-      "value": "red"
-    },
-    "security": {
-      "id": "10000"
-    },
     "environment": {
       "type": "doc",
       "version": 1,
@@ -109,31 +55,6 @@ const bodyData = `{
           ]
         }
       ]
-    },
-    "versions": [
-      {
-        "id": "10000"
-      }
-    ],
-    "duedate": "2019-05-11T00:00:00.000Z",
-    "customfield_60000": "jira-software-users",
-    "customfield_50000": {
-      "type": "doc",
-      "version": 1,
-      "content": [
-        {
-          "type": "paragraph",
-          "content": [
-            {
-              "text": "Could impact day-to-day work.",
-              "type": "text"
-            }
-          ]
-        }
-      ]
-    },
-    "assignee": {
-      "id": "5b109f2e9729b51b54dc274d"
     }
   }
 }`;
@@ -145,11 +66,19 @@ function sendTicketToJira() {
   const options = {
     method: 'POST',
     headers: {
-      'Authorization': `Basic `+btoa(credential.toString()),
+      'Authorization': `Basic ` + btoa(credential.toString()),
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: bodyData
   };
-  fetch('https://smithsfodddrge.atlassian.net/rest/api/3/issue', options);
+  fetch('https://smithsforge.atlassian.net/rest/api/3/issue', options)
+    .then(response => {
+      console.log(
+        `Response: ${response.status} ${response.statusText}`
+      );
+      return response.text();
+    })
+    .then(text => console.log(text))
+    .catch(err => console.error(err));
 }
